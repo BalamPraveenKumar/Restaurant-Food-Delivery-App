2bbb9b4dca9fc3e3a7a9920a057c3eff9b474246
BalamPraveenKumar <149877105+BalamPraveenKumar@users.noreply.github.com>
2026-02-08 21:01:34 +0530

    Add order flow, cart logic, and admin order management

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react"
-
+import axios from 'axios'
 export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
@@ -10,13 +10,30 @@ const StoreContextProvider = (props) => {
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([]);
 
+
+    const fetchFoodList = async () => {
+        const response = await axios.get(url + "/api/food/list")
+        setFoodList(response.data.data)
+    }
+    const loadCardData = async (token) => {
+        const response = await axios.post(url + "/api/cart/get", {}, { headers: { token } })
+        setCartItems(response.data.cartData);
+    }
+
+
     useEffect(() => {
-        const storedToken = localStorage.getItem("token");
-        if (storedToken) {
-            setToken(storedToken);
+        async function loadData() {
+            await fetchFoodList()
+            const storedToken = localStorage.getItem("token");
+            if (storedToken) {
+                setToken(storedToken);
+                await loadCardData(localStorage.getItem("token"))
+            }
+
         }
+        loadData();
     }, []);
-    const addToCart = (itemId) => {
+    const addToCart = async (itemId) => {
         if (!cartItems[itemId]) {
             setCartItems((prev) => ({
                 ...prev,
@@ -28,12 +45,22 @@ const StoreContextProvider = (props) => {
                 [itemId]: prev[itemId] + 1
             }))
         }
+        if (token) {
+            await axios.post(url + "/api/cart/add", { itemId }, { headers: { token } })
+
+        }
 
     }
-    const removeFromCart = (itemId) => {
+    const removeFromCart = async (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+        if (token) {
+            await axios.post(url + "/api/cart/remove", { itemId }, { headers: { token } })
+        }
 
     }
+
+
+
     const getTotalCartAmount = () => {
         let totoalAmount = 0;
         for (const item in cartItems) {
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,31 +1,92 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import { useContext } from 'react'
 import { StoreContext } from '../../context/StoreContext'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 const PlaceOrder = () => {
-  const { getTotalCartAmount } = useContext(StoreContext)
+  const { getTotalCartAmount, token, food_list, cartItems, url } = useContext(StoreContext)
   const navigate = useNavigate()
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: ""
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data => ({ ...data, [name]: value }))
+
+  }
+
+  const placeOrder = async (event) => {
+    event.preventDefault();
+    let orderItems = [];
+    food_list.map((item) => {
+      if (cartItems[item._id] > 0) {
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(itemInfo)
+      }
+
+    })
+
+    let orderData = {
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount() + 2,
+
+    }
+    let response = await axios.post(url + "/api/order/place", orderData, { headers: { token } })
+    if (response.data.success) {
+      const { session_url } = response.data
+      window.location.replace(session_url);
+    } else {
+      alert("Error");
+    }
+
+
+  }
+ 
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/cart")
+    }else if(getTotalCartAmount()==0){
+     navigate("/cart")
+    }
+  }, [token])
+
+
+
   return (
-    <form className='place-order'>
+    <form onSubmit={placeOrder} className='place-order'>
       <div className='place-order-left'>
         <p className='title'>Delivery Information</p>
         <div className='multi-fields'>
-          <input type='text' placeholder='First Name' />
-          <input type='text' placeholder='Last Name' />
+          <input required name='firstName' onChange={onChangeHandler} value={data.firstName} type='text' placeholder='First Name' />
+          <input required name='lastName' onChange={onChangeHandler} value={data.lastName} type='text' placeholder='Last Name' />
         </div>
-        <input type='text' placeholder='Email address' />
-        <input type='text' placeholder='Street' />
+        <input required name='email' onChange={onChangeHandler} value={data.email} type='text' placeholder='Email address' />
+        <input required name='street' onChange={onChangeHandler} value={data.street} type='text' placeholder='Street' />
 
         <div className='multi-fields'>
-          <input type='text' placeholder='City' />
-          <input type='text' placeholder='State' />
+          <input required name='city' onChange={onChangeHandler} value={data.city} type='text' placeholder='City' />
+          <input required name='state' onChange={onChangeHandler} value={data.state} type='text' placeholder='State' />
         </div>
         <div className='multi-fields'>
-          <input type='text' placeholder='Zip code' />
-          <input type='text' placeholder='Country' />
+          <input required name='zipcode' onChange={onChangeHandler} value={data.zipcode} type='text' placeholder='Zip code' />
+          <input required name='country' onChange={onChangeHandler} value={data.country} type='text' placeholder='Country' />
         </div>
-        <input type='text' placeholder='Phone number' />
+        <input required name='phone' onChange={onChangeHandler} value={data.phone} type='text' placeholder='Phone number' />
 
 
       </div>
@@ -48,7 +109,7 @@ const PlaceOrder = () => {
               <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
             </div>
           </div>
-          <button onClick={() => navigate('/order')}>PROCEED TO CHECKOUT</button>
+          <button type='submit' onClick={() => navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
 
       </div>
